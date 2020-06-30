5da9d4c66cda9b11b912d545f42ce75a4a7f9682
Alex Arroyo <alexarroyop31@gmail.com>
2020-06-30 16:32:32 -0500

    Prog001, Prog002, Prog003 y Prog004-Listo

---
+++ src/Estructuras/Prog004.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Estructuras;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Prog004 {
+    public static void main(String[] args) {
+        int numero = 1;
+        String mensaje = "Cristo Viene, prepárate...";
+        while (numero<=10){
+            System.out.println(mensaje);
+            numero++;
+        }
+    }
+}
