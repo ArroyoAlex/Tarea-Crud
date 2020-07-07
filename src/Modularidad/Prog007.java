c6818af35976e78d71796dd245c9214c9ab7f5c5
Alex Arroyo <alexarroyop31@gmail.com>
2020-07-07 14:15:00 -0500

    Programa 1 2 3 4 5 6 y 7 Listo

---
+++ src/Modularidad/Prog007.java
@@ -0,0 +1,20 @@
+
+package Modularidad;
+
+
+public class Prog007 {
+    public static void saludo(){
+        System.out.println("Binvenido al Sistema");
+    }
+    public static void despedida(){
+        System.out.println("Gracias por usar este programa");
+    }
+    public static void mensaje(){
+        System.out.println("En todo momento ama el amigo... Prov 17:17");
+    }
+    public static void main(String[] args) {
+        saludo();
+        mensaje();
+        despedida();
+    }
+}
